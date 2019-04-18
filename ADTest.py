98241599025b020d2d1433f66a587fd0abf3c64f
Luis Ángel (임 영민) <ultimate_luis@hotmail.com>
2019-04-17 23:47:41 -0700

    Added Autograd's Adam optimizer and epoch-based training.

--- ADTest.py
+++ ADTest.py
@@ -1,64 +1,64 @@
 """
 Test for gradient descent and automatic differentiation for linear regression.
-From https://github.com/mattnedrich/GradientDescentExample
+From https://github.com/mattnedrich/GradientDescentExample and https://github.com/HIPS/autograd/blob/master/examples/neural_net.py
 """
 
 import numpy as np
 import autograd
-
-cur_x = 0
-cur_y = 0
-grad_function = None
+from autograd.misc.optimizers import adam
+import matplotlib.pyplot as plt
 
 # y = mx + b
-# m is slope, b is y-intercept => w:[m, b]
-def compute_error_for_line_given_points( w, points ):
-	global cur_x, cur_y
+# m is slope, b is y-intercept => params:[m, b]
+def compute_error( params, inputs ):
 	totalError = 0
-	for i in range(0, len(points)):
-		cur_x = points[i, 0]
-		cur_y = points[i, 1]
-		totalError += loss_function( w )
-	return totalError / float(len(points))
+	for i in range( 0, len( inputs ) ):
+		x = inputs[i, 0]
+		y = inputs[i, 1]
+		totalError += ( y - ( np.dot( params, np.array( [x, 1.0] ) ) ) ) ** 2
+	return totalError / float( len( points ) )
 
-def loss_function( w ):
-	"""
-	Loss function for a single input pair of x, y values.
-	:param w: weights vector = [m, b]
-	:return: Squared error.
-	"""
-	global cur_x, cur_y
-	return (cur_y - ( w[0] * cur_x + w[1] )) ** 2
 
-def step_gradient( w, points, learningRate ):
-	global grad_function, cur_x, cur_y
-	w_grad = np.array([0.0, 0.0])
-	N = float( len( points ) )
-	for i in range( 0, len( points ) ):
-		cur_x = points[i, 0]
-		cur_y = points[i, 1]
-		w_grad += grad_function( w )
-	new_w = w - learningRate * w_grad / N
-	return new_w
-
-def gradient_descent_runner( points, w, learning_rate, num_iterations ):
-	weights = np.array( w )
-	for i in range(num_iterations):
-		weights = step_gradient( weights, np.array(points), learning_rate )
-	return weights
-
-def run():
-	global grad_function
+if __name__ == '__main__':
 	points = np.genfromtxt( "data.csv", delimiter="," )
-	learning_rate = 0.0001
-	grad_function = autograd.grad( loss_function )
-	initial_b = 0.0 								# Initial y-intercept guess.
-	initial_m = 0.0 								# Initial slope guess.
-	w = np.array( [initial_m, initial_b] )			# Initial weights.
-	num_iterations = 1000
-	print( "Starting gradient descent at b = {0}, m = {1}, error = {2}".format( initial_b, initial_m, compute_error_for_line_given_points( w, points ) ) )
-	print( "Running..." )
-	w = gradient_descent_runner( points, w, learning_rate, num_iterations )
-	print( "After {0} iterations b = {1}, m = {2}, error = {3}".format( num_iterations, w[1], w[0], compute_error_for_line_given_points( w, points ) ))
+	np.random.shuffle( points )
 
-run()
+	# Training parameters.
+	batch_size = 20
+	num_epochs = 25
+	num_batches = int( np.ceil( len( points ) / batch_size ) )
+	step_size = 0.1
+
+	def batch_indices( i ):
+		idx = i % num_batches
+		return slice( idx * batch_size, (idx + 1) * batch_size )
+
+	# Define training objective
+	def objective( params, i ):
+		idx = batch_indices( i )
+		return compute_error( params, points[idx] )
+
+	# Get gradient of objective using autograd.
+	objective_grad = autograd.grad( objective )
+
+	print( "     Epoch     |    Train accuracy  " )
+	def print_perf( params, i, gradient ):
+		if i % num_batches == 0:
+			train_acc = compute_error( params, points )
+			print( "{:15}|{:20}".format( i // num_batches, train_acc ) )
+
+
+	# The optimizers provided can optimize lists, tuples, or dicts of parameters.
+	optimized_params = adam( objective_grad, np.zeros( 2 ), step_size=step_size,
+							 num_iters=num_epochs * num_batches, callback=print_perf )
+	print_perf( optimized_params, 0, None )
+
+	plt.plot( points[:,0], points[:,1], "ro" )		# Plot points versus approximated line.
+	minx = np.min( points[:,0] )
+	maxx = np.max( points[:,0] )
+	miny = np.dot( optimized_params, np.array([minx, 1.0]) )
+	maxy = np.dot( optimized_params, np.array([maxx, 1.0]) )
+	plt.plot( [minx, maxx], [miny, maxy] )
+	plt.xlabel( "x" )
+	plt.ylabel( "y" )
+	plt.show()
