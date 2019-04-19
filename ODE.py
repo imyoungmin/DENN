675ed86dbd06d1c3fbe85b52ef28ebe12f4006ba
Luis Ángel (임 영민) <ultimate_luis@hotmail.com>
2019-04-18 20:35:15 -0700

    ODE works now.

--- ODE.py
+++ ODE.py
@@ -6,13 +6,10 @@ The analytical solution is y_a = exp(-x/5)*sin(x)
 The trial solution is y_t = x*N, where N = N(x, params) is a MLP.
 """
 
-import numpy as np
+import autograd.numpy as np
 import autograd
 from autograd.misc.optimizers import adam
 import matplotlib.pyplot as plt
-
-
-H = 10			# Number of neurons in hidden layer.
 
 
 def sigmoid( x ):
@@ -36,86 +33,138 @@ def sigmoidPrime( x ):
 def nnet( x, params ):
 	"""
 	Compute output from neural network.
-	:param x: A vector of n input values.
-	:param params: A list [(W,b),(V,)], where W is an mxn matrix (w_ij = weight from input j to neuron i), b is the bias vector with m elements, and V is a 1 x m weight vector.
+	:param x: Input value.
+	:param params: List [(W,b),(V,)], where W is an m-element vector (w_i = weight from unique input to neuron i), b is an m-element bias vector, and V is an m-element weight vector.
 	:return: Scalar evaluation of neural network.
 	"""
 	W = params[0][0]					# Weights towards hidden layer.
 	b = params[0][1]					# Biases for hidden layer units.
 	V = params[1][0]					# Weights towards output layer.
-	z = np.dot( W, x ) + b
+	z = W * x + b
 	sigma = sigmoid( z )				# Become inputs for output layer neuron.
 	return np.dot( V, sigma )			# Output neuron is linear.
 
 
-def dNnet_dxj( x, params, j ):
+def dNnet_dx( x, params ):
 	"""
-	Derivate of the network with respect to the jth entry.
-	:param x: Vector of n input values.
-	:param params: Network parameters (cfr nnet(.)).
-	:param j: Input index with respect to which we want to compute the derivative of nnet.
-	:return: N_g evaluated at x_j.
+	Derivate of the network with respect to the unique input value.
+	:param x: Input value.
+	:param params: Network parameters (cfr. nnet(.)).
+	:return: N_g evaluated at x.
 	"""
 	W = params[0][0]
 	b = params[0][1]
 	V = params[1][0]
-	z = np.dot( W, x ) + b
+	z = W * x + b
 	sigmaPrime = sigmoidPrime( z )
-	vw = V * W[:,j]
-	return np.dot( vw, sigmaPrime )
+	return np.dot( V * W, sigmaPrime )
 
 
-# y = mx + b
-# m is slope, b is y-intercept => params:[m, b]
-def compute_error( params, inputs ):
+def phi_a( x ):
+	"""
+	Analytical solution.
+	:param x: Input value.
+	:return: e^{-x/5}\sin(x)
+	"""
+	return np.exp( -x / 5.0 ) * np.sin( x )
+
+
+def phi_t( x, params ):
+	"""
+	Trial function.
+	:param x: Input value.
+	:param params: Neural network params (cfr. nnet(.)).
+	:return: Approximation to ODE solution.
+	"""
+	return x * nnet( x, params )
+
+
+def dPhi_t_dx( x, params ):
+	"""
+	Derivative of the trial function with respect to the input value
+	:param x: Input value.
+	:param params: Neural network params (cfr. nnet(.)).
+	:return: Derivative of phi_t w.r.t x evaluated at current input value.
+	"""
+	return nnet( x, params ) + x * dNnet_dx( x, params )
+
+
+def error( inputs, params ):
+	"""
+	Compute the average of the squared error.
+	:param inputs: Array of input values.
+	:param params: Neural network parameters.
+	:return: Avg squared error.
+	"""
 	totalError = 0
 	for i in range( 0, len( inputs ) ):
-		x = inputs[i, 0]
-		y = inputs[i, 1]
-		totalError += ( y - ( np.dot( params, np.array( [x, 1.0] ) ) ) ) ** 2
+		x = inputs[i]
+		totalError += ( dPhi_t_dx( x, params ) + 0.2 * phi_t( x, params ) - np.exp( -x / 5.0 ) * np.cos( x ) ) ** 2
 	return totalError / float( len( points ) )
 
 
 if __name__ == '__main__':
-	points = np.genfromtxt( "data.csv", delimiter="," )
-	np.random.shuffle( points )
+	np.random.seed( 0 )
+	N = 17										# Number of training samples.
+	MinX = 0
+	MaxX = 2
+	points = np.random.uniform( MinX, MaxX, N )	# Training dataset.
 
 	# Training parameters.
-	batch_size = 25
-	num_epochs = 100
+	H = 6  										# Number of neurons in hidden layer.
+	batch_size = 2
+	num_epochs = 30
 	num_batches = int( np.ceil( len( points ) / batch_size ) )
-	step_size = 0.1
+	step_size = 0.05
 
-	def batch_indices( i ):
+	def initParams():
+		return [(np.random.rand(H), np.random.rand(H)), (np.random.rand(H),)]
+
+	def batchIndices( i ):
 		idx = i % num_batches
 		return slice( idx * batch_size, (idx + 1) * batch_size )
 
 	# Define training objective
 	def objective( params, i ):
-		idx = batch_indices( i )
-		return compute_error( params, points[idx] )
+		idx = batchIndices( i )
+		return error( points[idx], params )
 
 	# Get gradient of objective using autograd.
 	objective_grad = autograd.grad( objective )
 
 	print( "     Epoch     |    Train accuracy  " )
-	def print_perf( params, i, _ ):
+	def printPerf( params, i, _ ):
 		if i % num_batches == 0:
-			train_acc = compute_error( params, points )
+			train_acc = error( points, params )
 			print( "{:15}|{:20}".format( i // num_batches, train_acc ) )
 
 
 	# The optimizers provided can optimize lists, tuples, or dicts of parameters.
-	optimized_params = adam( objective_grad, np.zeros( 2 ), step_size=step_size,
-							 num_iters=num_epochs * num_batches, callback=print_perf )
-	print_perf( optimized_params, 0, None )
+	optimizedParams = adam( objective_grad, initParams(), step_size=step_size,
+							 num_iters=num_epochs * num_batches, callback=printPerf )
+	printPerf( optimizedParams, 0, None )
 
-	plt.plot( points[:,0], points[:,1], "ro" )		# Plot points versus approximated line.
-	minx = np.min( points[:,0] )
-	maxx = np.max( points[:,0] )
-	miny = np.dot( optimized_params, np.array([minx, 1.0]) )
-	maxy = np.dot( optimized_params, np.array([maxx, 1.0]) )
-	plt.plot( [minx, maxx], [miny, maxy] )
+	# Plot analytical solution versus trial solution.
+	plt.figure( 1 )
+	p = np.linspace( MinX, MaxX, 100 )
+	y_a = phi_a( p )
+	plt.plot( p, y_a, "r-", label="Analytical solution" )
+	y_t = []
+	for x in p:
+		y_t += [phi_t( x, optimizedParams )]
+	plt.plot( p, y_t, "b-", label="Trial solution" )
+	plt.plot( [np.min( points ), np.min( points )], [0, 0.8], "g-" )
+	plt.plot( [np.max( points ), np.max( points )], [0, 0.8], "g-" )
 	plt.xlabel( "x" )
-	plt.ylabel( "y" )
+	plt.ylabel( r"$\phi(x)$" )
+	plt.legend()
+	plt.title( r"Approximating solution to $\frac{d\phi}{dx} + \frac{1}{5}\phi = e^{-x/5}\cos(x)$, $\phi(0) = 0$" )
+	plt.show()
+
+	# Plot error.
+	plt.figure( 2 )
+	plt.plot( p, np.abs( y_a - y_t ) )
+	plt.xlabel( "x" )
+	plt.ylabel( r"$|\phi_a(x) - \phi_t(x)|$" )
+	plt.title( "Error" )
 	plt.show()
